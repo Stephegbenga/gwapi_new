cf5c281622f025a8fd42c384272f281045907cfa
Stephen Gbenga <adegbamiyestephen2018@gmail.com>
2022-06-30 00:24:57 +0100

    update

---
+++ test.js
@@ -0,0 +1,6 @@
+var members_array = [];
+members_array.push({name: "John", age: 30});
+members_array.push({name: "Mary", age: 25});
+members_array.push({name: "Peter", age: 35});
+
+console.log(members_array);
