f1a308df18ada58a377afe6b2e9b84fca6c063e8
Stephen Gbenga <adegbamiyestephen2018@gmail.com>
2022-06-30 02:22:19 +0100

    update

---
+++ test.js
@@ -0,0 +1,241 @@
+var user = {
+    "Owner": {
+      "name": "APIANT_API APIANT_API",
+      "id": "5082809000011272001",
+      "email": "apiant_api@anahatayoga.com.hk"
+    },
+    "Email": "ellis.jayme@gmail.com",
+    "$currency_symbol": "HK$",
+    "$field_states": null,
+    "$photo_id": null,
+    "$review_process": {
+      "approve": false,
+      "reject": false,
+      "resubmit": false
+    },
+    "$canvas_id": null,
+    "MB_Active_Date": "2022-06-27",
+    "Name": "Teacher Training Course - Jayme Lynn Ellis",
+    "Last_Activity_Time": "2022-06-27T21:58:01+05:00",
+    "MB_Expiration_Date": "2022-11-30",
+    "Record_Image": null,
+    "Modified_By": {
+      "name": "Yogananth Andiappan",
+      "id": "5082809000000340001",
+      "email": "yogananth@anahatayoga.com.hk"
+    },
+    "$review": null,
+    "MB_Client_Membership_ID": "100642363",
+    "MB_Count": 30,
+    "MB_Payment_Date": "2022-06-27",
+    "$state": "save",
+    "Unsubscribed_Mode": null,
+    "$process_flow": false,
+    "MB_Program_Schedule_Type": "Class",
+    "MB_Membership_status": "active",
+    "MB_Program_ID": 30,
+    "id": "5082809000017297064",
+    "MB_Client_ID": "100025236",
+    "MB_Remaining": 30,
+    "$approved": true,
+    "$approval": {
+      "delegate": false,
+      "approve": false,
+      "reject": false,
+      "resubmit": false
+    },
+    "Modified_Time": "2022-06-27T21:58:01+05:00",
+    "Created_Time": "2022-06-27T21:57:34+05:00",
+    "Unsubscribed_Time": null,
+    "$editable": true,
+    "Mobile": "57101036",
+    "$orchestration": false,
+    "Contact": {
+      "module": "Contacts",
+      "name": "Jayme Lynn Ellis",
+      "id": "5082809000017318028"
+    },
+    "$in_merge": false,
+    "$status": "cmv_2-46",
+    "MB_Program_Name": "Andiappan Yoga",
+    "MB_Membership_ID": 23,
+    "Created_By": {
+      "name": "APIANT_API APIANT_API",
+      "id": "5082809000011272001",
+      "email": "apiant_api@anahatayoga.com.hk"
+    },
+    "Tag": [],
+    "$approval_state": "approved"
+  }
+
+
+  var arrays = [{
+    "name": "Owner name",
+    "value": "${user.Owner.name}"
+},  {
+    "name": "Owner id",
+    "value": "${user.Owner.id}"
+},  {
+    "name": "Owner email",
+    "value": "${user.Owner.email}"
+},  {
+    "name": "Email",
+    "value": "${user.Email}"
+},  {
+    "name": "$currency_symbol",
+    "value": "${user.$currency_symbol}"
+},  {
+    "name": "$field_states", 
+    "value": "${user.$field_states}"
+},  {
+    "name": "$photo_id",
+    "value": "${user.$photo_id}"
+},  {
+    "name": "$review_process approve",
+    "value": "${user.$review_process.approve}"
+},  
+{
+    "name": "$review_process reject",
+    "value": "${user.$review_process.reject}"
+}, 
+{
+    "name": "$review_process resubmit",
+    "value": "${user.$review_process.resubmit}"
+}, 
+
+{
+    "name": "$canvas_id",
+    "value": "${user.$canvas_id}"
+},  {
+    "name": "MB_Active_Date",
+    "value": "${user.MB_Active_Date}"
+},  {
+    "name": "Name",
+    "value": "${user.Name}"
+},  {
+    "name": "Last_Activity_Time",
+    "value": "${user.Last_Activity_Time}"
+},  {
+    "name": "MB_Expiration_Date",
+    "value": "${user.MB_Expiration_Date}"
+},  {
+    "name": "Record_Image",
+    "value": "${user.Record_Image}"
+},  {
+    "name": "Modified_By name",
+    "value": "${user.Modified_By.name}"
+},  {
+    "name": "Modified_By id",
+    "value": "${user.Modified_By.id}"
+},  {
+    "name": "Modified_By email",
+    "value": "${user.Modified_By.email}"
+},  {
+    "name": "$review",
+    "value": "${user.$review}"
+},  {
+    "name": "MB_Client_Membership_ID",
+    "value": "${user.MB_Client_Membership_ID}"
+},  {
+    "name": "MB_Count",
+    "value": "${user.MB_Count}"
+},  {
+    "name": "MB_Payment_Date",
+    "value": "${user.MB_Payment_Date}"
+},  {
+    "name": "$state",
+    "value": "${user.$state}"
+},  {
+    "name": "Unsubscribed_Mode",
+    "value": "${user.Unsubscribed_Mode}"
+},  {
+    "name": "$process_flow",
+    "value": "${user.$process_flow}"
+},  {
+    "name": "MB_Program_Schedule_Type",
+    "value": "${user.MB_Program_Schedule_Type}"
+},  {
+    "name": "MB_Membership_status",
+    "value": "${user.MB_Membership_status}"
+},  {
+    "name": "MB_Program_ID",
+    "value": "${user.MB_Program_ID}"
+},  {
+    "name": "id",
+    "value": "${user.id}"
+},  {
+    "name": "MB_Client_ID",
+    "value": "${user.MB_Client_ID}"
+},  {
+    "name": "MB_Remaining",
+    "value": "${user.MB_Remaining}"
+},  {
+    "name": "$approved",
+    "value": "${user.$approved}"
+},  {
+    "name": "$approval delegate",
+    "value": "${user.$approval.delegate}"
+},  {
+    "name": "$approval approve",
+    "value": "${user.$approval.approve}"
+},  {
+    "name": "$approval reject",
+    "value": "${user.$approval.reject}"
+},  {
+    "name": "$approval resubmit",
+    "value": "${user.$approval.resubmit}"
+},  {
+    "name": "Modified_Time",
+    "value": "${user.Modified_Time}"
+},  {
+    "name": "Created_Time",
+    "value": "${user.Created_Time}"
+},  {
+    "name": "Unsubscribed_Time",
+    "value": "${user.Unsubscribed_Time}"
+},  {
+    "name": "$editable",
+    "value": "${user.$editable}"
+},  {
+    "name": "Mobile",
+    "value": "${user.Mobile}"
+},  {
+    "name": "$orchestration",
+    "value": "${user.$orchestration}"
+}, 
+{
+ "name":"Contact module",
+    "value":"${user.Contact.module}"
+},
+ {
+    "name": "Contact name",
+    "value": "${user.Contact.name}"
+},  {
+    "name": "Contact id",
+    "value": "${user.Contact.id}"
+},  {
+    "name": "MB_Program_Name",
+    "value": "${user.MB_Program_Name}"
+},  {
+    "name": "MB_Membership_ID",
+    "value": "${user.MB_Membership_ID}"
+},  {
+    "name": "Created_By name",
+    "value": "${user.Created_By.name}"
+},  {
+    "name": "Created_By id",
+    "value": "${user.Created_By.id}"
+},  {
+    "name": "Created_By email",
+    "value": "${user.Created_By.email}"
+},  {"name":"$approval_state",
+    "value":"${user.$approval_state}"
+}
+
+ ]
+
+54
+
+// get the length of arrays
+var length = arrays.length;
+console.log(length);
