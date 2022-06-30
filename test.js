12128e1e41360cd44fa9edd46ec8755f1de9e621
Stephen Gbenga <adegbamiyestephen2018@gmail.com>
2022-06-30 01:50:38 +0100

    update

--- test.js
+++
@@ -1,6 +0,0 @@
-var members_array = [];
-members_array.push({name: "John", age: 30});
-members_array.push({name: "Mary", age: 25});
-members_array.push({name: "Peter", age: 35});
-
-console.log(members_array);
