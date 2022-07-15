c51c8b0503194347a80d370c3ce9b9768dcc061c
Stephegbenga <adegbamiyestephen2018@gmail.com>
2022-07-15 02:14:04 +0100

    update

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/second", (req, res) => {
+    res.sendFile(__dirname + "/public/secondpage.html");
 });
 
 
